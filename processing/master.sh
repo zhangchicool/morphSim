ddde96e31255fd9a10be39125a0567d1d79ed09e
Chi Zhang <zhangchicool@gmail.com>
2024-11-11 13:01:09 +0800

    organizing files

--- processing/master.sh
+++ processing/master.sh
@@ -2,10 +2,42 @@
 
 # master script
 
-for d in *-vs-*/
+for d in nonclock*/*-vs-mkv/
 do  
     echo "submitting job in $d"
     cd $d
-    bash run.sh &> cluster.log &
-    cd ..
+    cp ../../processing/sum_t.r .
+    cp ../../processing/cmd_un_mkv.nex cmd.nex
+    bash run.sh &
+    cd ../..
+done
+
+for d in nonclock*/*-vs-fkv/
+do  
+    echo "submitting job in $d"
+    cd $d
+    cp ../../processing/sum_t.r .
+    cp ../../processing/cmd_un_fkv.nex cmd.nex
+    bash run.sh &
+    cd ../..
+done
+
+for d in tipdating*/*-vs-mkv/
+do  
+    echo "submitting job in $d"
+    cd $d
+    cp ../../processing/sum_t.r .
+    cp ../../processing/cmd_td_mkv.nex cmd.nex
+    bash run.sh &
+    cd ../..
+done
+
+for d in tipdating*/*-vs-fkv/
+do  
+    echo "submitting job in $d"
+    cd $d
+    cp ../../processing/sum_t.r .
+    cp ../../processing/cmd_td_fkv.nex cmd.nex
+    bash run.sh &
+    cd ../..
 done
