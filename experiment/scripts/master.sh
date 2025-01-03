9a4acfba60a07c6cd2adba96331bc16a38f395cd
Chi Zhang <zhangchicool@gmail.com>
2025-01-03 12:53:57 +0800

    organizing files

--- experiment/scripts/master.sh
+++ experiment/scripts/master.sh
@@ -6,8 +6,8 @@ for d in nonclock*/*-vs-mkv/
 do  
     echo "submitting job in $d"
     cd $d
-    cp ../../processing/sum_t.r .
-    cp ../../processing/cmd_un_mkv.nex cmd.nex
+    cp ../../scripts/sum_t.r .
+    cp ../../scripts/cmd_un_mkv.nex cmd.nex
     bash run.sh &
     cd ../..
 done
@@ -16,8 +16,8 @@ for d in nonclock*/*-vs-fkv/
 do  
     echo "submitting job in $d"
     cd $d
-    cp ../../processing/sum_t.r .
-    cp ../../processing/cmd_un_fkv.nex cmd.nex
+    cp ../../scripts/sum_t.r .
+    cp ../../scripts/cmd_un_fkv.nex cmd.nex
     bash run.sh &
     cd ../..
 done
@@ -26,8 +26,8 @@ for d in tipdating*/*-vs-mkv/
 do  
     echo "submitting job in $d"
     cd $d
-    cp ../../processing/sum_t.r .
-    cp ../../processing/cmd_td_mkv.nex cmd.nex
+    cp ../../scripts/sum_t.r .
+    cp ../../scripts/cmd_td_mkv.nex cmd.nex
     bash run.sh &
     cd ../..
 done
@@ -36,8 +36,8 @@ for d in tipdating*/*-vs-fkv/
 do  
     echo "submitting job in $d"
     cd $d
-    cp ../../processing/sum_t.r .
-    cp ../../processing/cmd_td_fkv.nex cmd.nex
+    cp ../../scripts/sum_t.r .
+    cp ../../scripts/cmd_td_fkv.nex cmd.nex
     bash run.sh &
     cd ../..
 done
