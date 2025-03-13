d1a02e0df1cd7c8d35aeab070fdac6a5c23a26fd
Chi Zhang <zhangchicool@gmail.com>
2025-03-13 10:54:24 +0800

    dealing with results

---
+++ scripts/summary.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+for d in nonclock*/*-vs-*/
+do  
+    echo "working in $d"
+    cd $d  
+    cp ../../scripts/sum_t.r .
+    
+    # distance metrics
+    echo "reso  sja  d_qt  d_ci  d_rf" > dist_t.txt
+    for i in {1..100}
+    do
+    	sed -n "$i"p  ../../simulator/bd.trees > bd.tre
+    	mv sim_$i.con.tre data.nex.con.tre
+    	Rscript sum_t.r >> ../../out.log 2>&1
+    	mv data.nex.con.tre sim_$i.con.tre
+    done
+
+    # tree lengths
+    grep "      TL"      run.log > tl_estm.txt
+
+    rm bd.tre
+    rm sum_t.r
+    cd ../..
+done
+
+for d in tipdating*/*-vs-*/
+do  
+    echo "working in $d"
+    cd $d  
+    cp ../../scripts/sum_t.r .
+    
+    # distance metrics
+    echo "reso  sja  d_qt  d_ci  d_rf" > dist_t.txt
+    for i in {1..100}
+    do
+    	sed -n "$i"p  ../../simulator/bd.trees > bd.tre
+    	mv sim_$i.con.tre data.nex.con.tre
+    	Rscript sum_t.r >> ../../out.log 2>&1
+    	mv data.nex.con.tre sim_$i.con.tre
+    done
+
+    # tree heights and clock rates
+    grep "     age\[0\] " run.log > th_estm.txt
+    grep "     clockrate" run.log > cl_base.txt
+    grep "     ilnvar   " run.log > cl_var.txt
+
+    rm bd.tre
+    rm sum_t.r
+    cd ../..
+done
