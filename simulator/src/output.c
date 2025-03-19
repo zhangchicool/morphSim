2a266bb6107abcbd4ec2d7ac9751ddbb012ea6b4
Chi Zhang <zhangchicool@gmail.com>
2025-03-19 11:12:19 +0800

    used different missing proportion for extinct and extant taxa

--- simulator/src/output.c
+++ simulator/src/output.c
@@ -17,11 +17,21 @@ void writeMrBayesCmd(FILE* fp, pPhyTree tree, double missing) {
     if (tree->nsites > 0) {
         for (i = 0; i < tree->ntips; i++) {
             fprintf(fp, "  %s\t", tree->tips[i]->name);
-            for (j = 0; j < tree->nsites; j++) {
-                if (rndu() < missing && tree->tips[i]->age > 1E-8)
-                    fprintf(fp, "?");
-                else
-                    fprintf(fp, "%d", tree->tips[i]->sequence[j]);
+            if (tree->tips[i]->age > 1E-8) {
+                for (j = 0; j < tree->nsites; j++) {
+                    if (rndu() < missing)
+                        fprintf(fp, "?");
+                    else
+                        fprintf(fp, "%d", tree->tips[i]->sequence[j]);
+                }
+            }
+            else {  // extant taxa
+                for (j = 0; j < tree->nsites; j++) {
+                    if (rndu() < missing/5.0)  // less missing states
+                        fprintf(fp, "?");
+                    else
+                        fprintf(fp, "%d", tree->tips[i]->sequence[j]);
+                }
             }
             fprintf(fp, "\n");
         }
