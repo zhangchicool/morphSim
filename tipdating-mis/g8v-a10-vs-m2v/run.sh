1b15146dfa4e59204ab7667497762b4d14a0e3b4
Chi Zhang <zhangchicool@gmail.com>
2025-03-26 11:21:20 +0800

    results from data with missing states

---
+++ tipdating-mis/g8v-a10-vs-m2v/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "G8v(a=10) -vs- M2v" > run.log
+
+for i in {1..100}
+do
+  echo "** tree $i **" >> run.log
+  
+  # get the i-th complete birth-death tree
+  sed -n "$i"p  ../../simulator/bd.trees > bd.tre
+  
+  # generate a data file 
+  ../../msim -i bd.tre -o data.nex -l 200 -m 0.5 -a 10 -r 10 -q 3 >> run.log
+  
+  # run mrbayes to infer the parameters
+  ../../mb cmd.nex >> run.log
+  
+  # rename files to avoid overwriting
+  mv data.nex        data_$i.nex
+  mv data.nex.con.tre sim_$i.con.tre
+done
